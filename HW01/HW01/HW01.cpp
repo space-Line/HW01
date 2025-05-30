13c791b72a93868f56dcc93969b11e8f29a4eca6
space-Line <rlatjsdn553@naver.com>
2025-05-30 14:09:53 +0900

    hw01

--- HW01/HW01/HW01.cpp
+++ HW01/HW01/HW01.cpp
@@ -1,6 +1,28 @@
 ﻿#include <iostream>
 
+int total(int arr[]) {
+    int total = 0;
+    for (int i = 0; i < 5; i++) total += arr[i];
+
+    return total;
+}
+
+double average(int sum) {
+    double avg = sum / 5;
+
+    return avg;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int arr[5];
+
+    std::cout << "5개의 숫자를 입력하세요 : ";
+    for (int i = 0; i < 5; i++) {
+        std::cin >> arr[i];
+    }
+    int sum = total(arr);
+    double avg = average(sum);
+
+    std::cout << "합계 : " << sum << "\n평균 : " << avg;
 }
